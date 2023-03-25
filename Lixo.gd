9f7c513daa11c9060bf54d8e1cfe32eb8f3d6583
Bruno Dalagnol <dalagnolbruno88@gmail.com>
2023-03-25 18:08:19 -0300

    adicionando no de lixo

---
+++ Lixo.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+@export var tempoDeVida: float = 10000
+@export var degradacao: float = .2
+@export var tempoDegradacao: float = 0.10
+
+# caixa garrafa saco
+const type: String = "caixa"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
