ad734f6857166f2b9e910c40db34cc9f768842a2
Bruno Dalagnol <dalagnolbruno88@gmail.com>
2023-03-25 19:41:51 -0300

    adicionando geração de lixo

---
+++ Raiz.gd
@@ -0,0 +1,28 @@
+extends Node
+
+const CenaLixo: PackedScene = preload("res://lixo.tscn")
+
+@export var geracaoLixo: int = 40
+@export var geracaoLixoPadding: float = 100
+@export var modGeracaoLixo: int = 5
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var contagemLixo = randi_range(geracaoLixo-modGeracaoLixo, geracaoLixo+modGeracaoLixo)
+	print("instanciando: ", contagemLixo, " lixo...")
+	
+	var window_size: Vector2 = get_window().size
+	
+	for i in range(contagemLixo):
+		var lixo = CenaLixo.instantiate()
+		
+		lixo.position = Vector2(
+			randf_range(geracaoLixoPadding, window_size[0]-geracaoLixoPadding),
+			randf_range(geracaoLixoPadding, window_size[1]-geracaoLixoPadding)
+			)
+		add_child(lixo)
+	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
