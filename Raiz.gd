e66277bf953312b52f3f7e196ed578398c99c65c
witchwarlock666 <87949744+witchwarlock666@users.noreply.github.com>
2023-03-26 00:48:15 -0300

    Corrigido erro 'flip_w'

--- Raiz.gd
+++ Raiz.gd
@@ -13,14 +13,14 @@ func _ready():
 	
 	var window_size: Vector2 = get_window().size
 	
-#	for i in range(contagemLixo):
-#		var lixo = CenaLixo.instantiate()
-#
-#		lixo.position = Vector2(
-#			randf_range(geracaoLixoPadding, window_size[0]-geracaoLixoPadding),
-#			randf_range(geracaoLixoPadding, window_size[1]-geracaoLixoPadding)
-#			)
-#		add_child(lixo)
+	for i in range(contagemLixo):
+		var lixo = CenaLixo.instantiate()
+
+		lixo.position = Vector2(
+			randf_range(geracaoLixoPadding, window_size[0]-geracaoLixoPadding),
+			randf_range(geracaoLixoPadding, window_size[1]-geracaoLixoPadding)
+			)
+		add_child(lixo)
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
