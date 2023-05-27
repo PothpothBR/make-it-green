022f02c6a4bcbb886c0ca3f71aa8f5bf544fb88b
witchwarlock666 <87949744+witchwarlock666@users.noreply.github.com>
2023-05-27 20:37:07 -0300

    Base carregar dados

--- save.gd
+++ save.gd
@@ -13,3 +13,22 @@ static func salvar(save):
 	db.insert_row("progressao_jogador", save["progressao_jogador"])
 	
 	db.close_db()
+	
+static func carregar(save):
+	var db = SQLite.new()
+	db.path = "res://master"
+	db.verbosity_level = SQLite.NORMAL
+	db.open_db()
+	
+	var selected = db.select_rows("saves", "", ["data", "dificuldade"])
+	if selected.is_empty():
+		db.close_db()
+		return save
+	save["saves"] = selected[0]
+	
+	selected = db.select_rows("progressao_jogador", "id_saves = 1", ["id_saves", "nome", "points", "x", "y"])
+	save["progressao_jogador"] = selected[0]
+	
+	db.close_db()
+	
+	return save
