bdf7d276a99d7d65bc748ef4828c633736fd59d0
Lucas Guimaraes Moreira <103656598+JkaiPrime@users.noreply.github.com>
2023-06-24 10:01:49 -0300

    feito a ligação da loja com o inventario

--- loja.gd
+++ loja.gd
@@ -2,64 +2,47 @@ extends CanvasLayer
 
 var gameState
 #itens do inventario
-var inventario
+var inventarioItens
+
 func _ready():
-#	self.inventario = get_node("/root").inventario
 	pass
-func create_texture():
-	#criando uma nova instancia de atlas texture
-	var texture = AtlasTexture.new()
-	#carregando o path 
-	texture.atlas = load("res://resources/Sprit-Itens.png")
-	#criando a regioa vazia
-	texture.region = Rect2()
-	#retornando o texture
-	return texture
-
-func delimitando_regiao_atlas(texture_atlas, sprite, rect):
-	var regiao = texture_atlas.atlas_rect_region(rect)
-	texture_atlas.add_region(sprite, regiao)
-
-func usar_atlas():
-	#criando um novo sprite
-	var sprite = Sprite2D.new()
-	#chamando a função create texture
-	var texture_atlas = create_texture()
-	#configurando o sprite
-	sprite.texture = texture_atlas
-	sprite.set_texture(texture_atlas)
-	# Define a região do atlas para o sprite
-	sprite.set_region(Rect2(0, 0, 64, 64))
-	add_child(sprite)
-	# Defina o retângulo desejado para delimitar a região do atlas
-	var rect = Rect2(0, 0, 64, 64)	
-	# Chama a função para delimitar a região do atlas
-	delimitando_regiao_atlas(texture_atlas, sprite, rect)  
-
-
-
 
 #-----------------itens de plantação-----------------
 func on_comprar_semente_arvore():
-	#saber qual item tem que ser carregado
-	#criar um atlas texture programaticamente
-	#configurar o atlas texture(carregar arquivo de sprites,selecionar a regiao do item)
-	#achar inventario com get_node("/root").inventario
-	#adcionar um item ao inventario(nome,atlas texture)
-	pass
+	var texture  = AtlasTexture.new()
+	texture.set_atlas(load("res://resources/Sprit-Itens.png"))
+	#adcionar as cordenadas
+	texture.set_region(Rect2(0, 0, 32, 32))
+	
+	inventarioItens.add_item("josias",texture)
+
+	
+
+	
+
 func on_comprar_semente_arbusto():
-	pass
+	var texture  = AtlasTexture.new()
+	var sprite = Sprite2D.new()
 func on_comprar_regador():
-	pass
+	var texture  = AtlasTexture.new()
+	var sprite = Sprite2D.new()
+	
 func on_comprar_fertilizante():
-	pass
+	var texture  = AtlasTexture.new()
+	var sprite = Sprite2D.new()
 #-----------------Mochilas-----------------
 func on_comprar_mochila_1():
-	pass
+	var texture  = AtlasTexture.new()
+	var sprite = Sprite2D.new()
 func on_comprar_mochila_2():
-	pass
+	var texture  = AtlasTexture.new()
+	var sprite = Sprite2D.new()
+
 func on_comprar_mochila_3():
-	pass
+	var texture  = AtlasTexture.new()
+	var sprite = Sprite2D.new()
+
 func on_comprar_mochila_4():
-	pass
+	var texture  = AtlasTexture.new()
+	var sprite = Sprite2D.new()
 
